f919900215a585f9f3b649c7f272d2f0c32c9353
Felix Chen <siyuan-c23@mails.tsinghua.edu.cn>
2025-11-04 18:15:02 +0800

    initialization

---
+++ utils/math_function.py
@@ -0,0 +1,33 @@
+def extract_box(pred_str):
+    ans = pred_str.split("boxed")[-1]
+    if len(ans) == 0:
+        return ""
+    elif ans[0] == "{":
+        stack = 1
+        a = ""
+        for c in ans[1:]:
+            if c == "{":
+                stack += 1
+                a += c
+            elif c == "}":
+                stack -= 1
+                if stack == 0:
+                    break
+                a += c
+            else:
+                a += c
+    else:
+        a = ans.split("$")[0].strip()
+
+    return a
+
+def trim_output(output):
+    instruction_prefix = "Answer the following question"
+    question_prefix = 'Question:'
+    comment_prefix = 'Comment:'  # for some reason, Llama 13B likes to generate these comments indefinitely
+
+    for prefix in [instruction_prefix, question_prefix, comment_prefix]:
+        if prefix in output:
+            output = output.split(prefix)[0]
+
+    return output
